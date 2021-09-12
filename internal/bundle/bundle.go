cc0ca3d7bb65e4c79e3a6edc89e7dbf0f41b8f8a
Neo Hsu <NeoHsuDroid@gmail.com>
2021-09-12 14:36:06 +0800

    test

--- internal/bundle/bundle.go
+++ internal/bundle/bundle.go
@@ -2,7 +2,9 @@ package bundle
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 
 	"github.com/NeoHsu/project-CI/internal/handlers"
 	"github.com/NeoHsu/project-CI/internal/health"
@@ -20,7 +22,15 @@ func registerHooks(lifecycle fx.Lifecycle, h *handlers.HttpHandler) {
 		fx.Hook{
 			OnStart: func(context.Context) error {
 				fmt.Println("Starting application on 9000")
-				go h.Engine.Run(":9000")
+				go func() {
+					if err := h.Engine.Run(":9000"); err != nil {
+						if errors.Is(err, http.ErrServerClosed) {
+							fmt.Println("Shutting down the Application")
+						} else {
+							fmt.Printf("Error to Start Application: %v", err)
+						}
+					}
+				}()
 				return nil
 			},
 			OnStop: func(context.Context) error {
